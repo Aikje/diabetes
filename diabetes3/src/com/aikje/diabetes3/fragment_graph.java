627987c6b68e8176ed74c4f4baeb3bd920428beb
Aikje <aike@live.nl>
2014-05-16 12:12:24 +0200

    In plaats van androidPlot gebruiken we nu Android Graphview. Dit zou beter moeten werken met fragments. Huidige versie crasht op fragment_graph.

--- diabetes3/src/com/aikje/diabetes3/fragment_graph.java
+++ diabetes3/src/com/aikje/diabetes3/fragment_graph.java
@@ -5,6 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.GraphView.*;
 
 public class fragment_graph extends Fragment{
 
@@ -17,11 +23,25 @@ public class fragment_graph extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-
 		View rootView = inflater.inflate(R.layout.fragment_graph, container, false);
+		
+		// init example series data
+		GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+		    new GraphViewData(1, 2.0d)
+		    , new GraphViewData(2, 1.5d)
+		    , new GraphViewData(3, 2.5d)
+		    , new GraphViewData(4, 1.0d)
+		});
+		 
+		GraphView graphView = new LineGraphView(
+		    getActivity() // context
+		    , "GraphViewDemo" // heading
+		);
+		graphView.addSeries(exampleSeries); // data
+		 
+		LinearLayout layout = (LinearLayout) getView().findViewById(R.id.graph1);
+		layout.addView(graphView);
 
 		return rootView;
-
 	}
-	
 }
